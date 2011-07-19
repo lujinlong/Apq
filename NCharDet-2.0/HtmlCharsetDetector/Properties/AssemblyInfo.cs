85a8779c86a560cbdfa06f644cfc43fa946eee5e
amwpfiqvy <amwpfiqvy@163.com>
2011-07-19 12:01:24 +0800

    NCharDet-2.0
    Signed-off-by: amwpfiqvy <amwpfiqvy@163.com>

---
+++ NCharDet-2.0/HtmlCharsetDetector/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿/*
+ Use under license
+ http://www.mozilla.org/MPL/MPL-1.1.txt
+ */
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HtmlCharsetDetector Console Application")]
+[assembly: AssemblyDescription("http://www.mozilla.org/MPL/     http://www.conceptdevelopment.net")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HtmlCharsetDetector")]
+[assembly: AssemblyCopyright("2004-2007 Craig Dunn")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("df7a8374-7bc6-42f7-9d10-428e71aee458")]
